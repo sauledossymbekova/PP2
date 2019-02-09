22f2f864d066b8214822632f8643869c82050e30
Saule <sauledossymbekova@gmail.com>
2019-02-09 19:08:22 +0600

    lab

---
+++ week1/1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine(); // number of first line elements
+            string line = Console.ReadLine(); // numbers as second line  1 2 3 4 5
+            string[]arr = line.Split(' '); // 'line' as array; each element is splitted by ' '
+            int k=0; // epmty value for counting number of prime numbers
+            string res = ""; // empty string for result string
+            bool b;
+            foreach (var x in arr) // running to each element of array            
+            {
+                b = true;
+                int y = int.Parse(x); // t as number of elements in array
+                if (y == 1)
+                    b = false;
+                for (int i = 2; i*i <= y; i++) //algorithm for checking elements are prime; 1,4
+                {
+                    if (y % i == 0) // if no remainder it is not prime
+                    {
+                        b = false; // so false
+                    }
+                }
+            if (b) // if there are numbers well gone through algor. so it is prime 
+                {
+                    k++; // counts +1
+                    res = res + " " + x; // will add prime numbers in empty string
+
+                }
+            }
+            Console.WriteLine(k);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string n1 = Console.ReadLine();
+            int n = int.Parse(n1);
+            string a = "[*]";
+            for (int i=0; i<n; i++)
+            {
+                Console.WriteLine(a+" ");
+                
+                Console.WriteLine("");
+                
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
